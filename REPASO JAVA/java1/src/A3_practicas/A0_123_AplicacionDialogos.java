a921a064c9cb98802b0dbd8824571e38bb0f12c0
Boo <hpmajin@outlook.es>
2021-07-22 21:20:11 -0500

    repaso java full v.5

---
+++ REPASO JAVA/java1/src/A3_practicas/A0_123_AplicacionDialogos.java
@@ -0,0 +1,15 @@
+package A3_practicas;
+
+import javax.swing.JFrame;
+
+public class A0_123_AplicacionDialogos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		A1_123_MarcoDialogos marco1 = new A1_123_MarcoDialogos();
+		marco1.setVisible(true);
+		marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
